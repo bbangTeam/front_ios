ca04835173e292a282b34865f812dbaac9390882
bart <bartshin@icloud.com>
2021-05-23 21:12:58 +0700

    First init

---
+++ Bbang/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Bbang
+//
+//  Created by bart Shin on 23/05/2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
