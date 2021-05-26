c8098586e04c965d990986b7773a224149caf9a1
so0nim <308go@naver.com>
2021-05-26 15:17:21 +0900

    add 설정 탭바

---
+++ Bbang/설정/BbangsetViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BbangsetViewController.swift
+//  Bbang
+//
+//  Created by 소영 on 2021/05/26.
+//
+
+import UIKit
+
+class BbangsetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
