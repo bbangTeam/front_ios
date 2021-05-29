ba86418e1fa95b31cd54fa5cae0b7217662aa140
so0nim <308go@naver.com>
2021-05-29 20:45:15 +0900

    빵스타그램 collectionView 기본 세팅

---
+++ Bbang/빵스타그램/BbangstaCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  BbangstaCollectionViewCell.swift
+//  Bbang
+//
+//  Created by 소영 on 2021/05/29.
+//
+
+import UIKit
+
+class BbangstaCollectionViewCell: UICollectionViewCell {
+    
+    
+}
--- Bbang/빵스타그램/BbangstagramViewController.swift
+++ Bbang/빵스타그램/BbangstagramViewController.swift
@@ -9,21 +9,62 @@ import UIKit
 
 class BbangstagramViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+    
+    @IBOutlet var BbangstaCollectionView: UICollectionView!
     
 
-    /*
-    // MARK: - Navigation
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        BbangstaCollectionView.delegate = self
+        BbangstaCollectionView.dataSource = self
+        
+ //       BbangstaCollectionView.register(BbangstaCollectionViewCell.self, forCellWithReuseIdentifier: "BbangstaCollectionViewCell")
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
+    
+}
 
+extension BbangstagramViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 9
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "BbangstaCollectionViewCell", for: indexPath) as! BbangstaCollectionViewCell
+        
+        cell.backgroundColor = .blue
+
+        return cell
+    }
+    
+    
+}
+
+extension BbangstagramViewController: UICollectionViewDelegateFlowLayout {
+    
+    // 위 아래 간격
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        
+        return 1
+    }
+    
+    // 옆 간격
+        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+            
+            return 1
+        }
+    
+    // cell 사이즈
+        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+            
+            let width = collectionView.frame.width
+            let size = CGSize(width: width, height: width)
+            
+            return size
+            
+        }
+    
+    
 }
