d38929e2b3cffbe73bcce5350b7ec09403ee7b02
so0nim <308go@naver.com>
2021-06-12 19:53:42 +0900

    modify 빵지순례 레이아웃

---
+++ Bbang/빵지순례/BbangtourTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  BbangtourTableViewCell.swift
+//  Bbang
+//
+//  Created by 소영 on 2021/06/12.
+//
+
+import UIKit
+
+class BbangtourTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
