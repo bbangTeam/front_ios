76e1cf2c3492ff84a9ce06d5dae70a2d366c1b90
so0nim <308go@naver.com>
2021-06-26 00:29:33 +0900

    modify 빵지순례 tableView 동적 높이 구현

--- Bbang/빵지순례/BbangtourViewController.swift
+++ Bbang/빵지순례/BbangtourViewController.swift
@@ -1,23 +1,42 @@
 //
-//  BbangtourViewController.swift
+//  testViewController.swift
 //  Bbang
 //
-//  Created by 소영 on 2021/05/26.
+//  Created by 소영 on 2021/06/25.
 //
 
 import UIKit
 
 class BbangtourViewController: UIViewController {
     
-    
+    let count = 10
+
     @IBOutlet var bbangtourTableView: UITableView!
+    @IBOutlet var bbangtourTableViewHeight: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        bbangtourTableView.delegate = self
-        bbangtourTableView.dataSource = self
         
+        bbangtourTableView.dataSource = self
+        bbangtourTableView.delegate = self
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.bbangtourTableView.addObserver(self, forKeyPath: "contentSize", options: .new, context: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        self.bbangtourTableView.removeObserver(self, forKeyPath: "contentSize")
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "contentSize" {
+            if let newvalue = change?[.newKey]{
+                let newsize = newvalue as! CGSize
+                self.bbangtourTableViewHeight.constant = newsize.height
+            }
+        }
     }
     
 
@@ -27,22 +46,32 @@ class BbangtourViewController: UIViewController {
 
 extension BbangtourViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "BbangtourTableViewCell", for: indexPath) as! BbangtourTableViewCell
-        
-        cell.bbangtourImageView.layer.cornerRadius = 8
-        
-        cell.bbangtourBreadTimeLabel.layer.masksToBounds = true
-        cell.bbangtourBreadTimeLabel.layer.cornerRadius = 2
-        cell.bbangtourStoreTimeLabel.layer.masksToBounds = true
-        cell.bbangtourStoreTimeLabel.layer.cornerRadius = 2
-        
-        
-        return cell
-        
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "BbangtourTableViewCell", for: indexPath) as? BbangtourTableViewCell {
+            
+            cell.bbangtourImageView.layer.cornerRadius = 8
+            
+            cell.bbangtourBreadTimeLabel.layer.masksToBounds = true
+            cell.bbangtourBreadTimeLabel.layer.cornerRadius = 2
+            cell.bbangtourStoreTimeLabel.layer.masksToBounds = true
+            cell.bbangtourStoreTimeLabel.layer.cornerRadius = 2
+            
+            return cell
+    
+        }
+        return UITableViewCell()
     }
     
 }
