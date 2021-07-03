4d80416ee0c1a6debf398f0ee92d22d2953a8c55
so0nim <308go@naver.com>
2021-07-04 00:35:56 +0900

    add 빵스타그램 글쓰기 레이아웃

--- Bbang/빵스타그램/BbangstaWriteViewController.swift
+++ Bbang/빵스타그램/BbangstaWriteViewController.swift
@@ -9,21 +9,70 @@ import UIKit
 
 class BbangstaWriteViewController: UIViewController {
 
+    @IBOutlet var locationTextField: UITextField!
+    @IBOutlet var storeTextField: UITextField!
+    @IBOutlet var breadNameTextField: UITextField!
+    
+    @IBOutlet var breadReviewTextView: UITextView!
+    
+    @IBOutlet var cameraButton: UIButton!
+    @IBOutlet var cameraView: UIView!
+    @IBOutlet var photoCollectionView: UICollectionView!
+    @IBOutlet var writeButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        photoCollectionView.delegate = self
+        photoCollectionView.dataSource = self
+        
+        breadReviewTextView.layer.borderWidth = 1
+        breadReviewTextView.layer.cornerRadius = 4
+        breadReviewTextView.layer.borderColor = #colorLiteral(red: 0.9607843137, green: 0.9607843137, blue: 0.9607843137, alpha: 1)
+        cameraButton.layer.borderWidth = 1
+        cameraButton.layer.cornerRadius = 4
+        cameraButton.layer.borderColor = #colorLiteral(red: 0.9607843137, green: 0.9607843137, blue: 0.9607843137, alpha: 1)
+        cameraView.layer.cornerRadius = 10
+        writeButton.layer.cornerRadius = 8
+        
+        let photoCollectionViewFlowLayout = UICollectionViewFlowLayout()
+        photoCollectionView.collectionViewLayout = photoCollectionViewFlowLayout
+        photoCollectionViewFlowLayout.scrollDirection = .horizontal
+        
+    }
+    
+    @IBAction func writeButtonAction(_ sender: UIButton) {
     }
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+//MARK: - CollectionView
+extension BbangstaWriteViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
     }
-    */
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "WritePhotoCollectionViewCell", for: indexPath)
+        
+        
+        return cell
+    }
+    
+    
+}
 
+//MARK: CollectionView FlowLayout
+extension BbangstaWriteViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 70, height: 70)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 8
+    }
+    
 }
---
+++ Bbang/빵스타그램/WritePhotoCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  WritePhotoCollectionViewCell.swift
+//  Bbang
+//
+//  Created by 소영 on 2021/07/04.
+//
+
+import UIKit
+
+class WritePhotoCollectionViewCell: UICollectionViewCell {
+    
+}
