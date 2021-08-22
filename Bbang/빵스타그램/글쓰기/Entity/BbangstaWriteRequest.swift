d7fa91aff81ccc7b3e0aaeff9ec3b5415b007ad7
so0nim <308go@naver.com>
2021-08-22 21:33:16 +0900

    -

---
+++ Bbang/빵스타그램/글쓰기/Entity/BbangstaWriteRequest.swift
@@ -0,0 +1,8 @@
+//
+//  BbangstaWriteRequest.swift
+//  Bbang
+//
+//  Created by 소영 on 2021/08/22.
+//
+
+import Foundation
---
+++ Bbang/빵스타그램/글쓰기/Entity/BbangstaWriteResponse.swift
@@ -0,0 +1,8 @@
+//
+//  BbangstaWriteResponse.swift
+//  Bbang
+//
+//  Created by 소영 on 2021/08/22.
+//
+
+import Foundation
